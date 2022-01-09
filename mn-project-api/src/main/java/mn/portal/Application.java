66ae467ee7e8d02fa499a9e04438a11ba70fcec9
Krzysztof Łopuch <klopuch@gmail.com>
2022-01-09 14:10:53 +0100

    added swagger ui

--- mn-project-api/src/main/java/mn/portal/Application.java
+++ mn-project-api/src/main/java/mn/portal/Application.java
@@ -1,7 +1,20 @@
 package mn.portal;
 
+import io.micronaut.openapi.annotation.OpenAPIInclude;
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MN",
+                version = "1.0",
+                description = "MN API"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
